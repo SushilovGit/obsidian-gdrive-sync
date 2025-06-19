bc47c5eaef9bf9cda4683e576084164a139b4ae9
SushilovGit <inbox.snd@gmail.com>
2025-06-19 18:27:58 +0300

    c-190625

---
+++ main.js
@@ -0,0 +1,18 @@
+'use strict'
+var obsidian = require('obsidian')
+
+class GoogleDriveSync extends obsidian.Plugin {
+    async onload() {
+
+    }
+}
+
+
+
+function uploadAppdata() {
+
+}
+
+
+
+module.exports = GoogleDriveSync
